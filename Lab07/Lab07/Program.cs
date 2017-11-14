a274c0f1eddfbf7820b7838d43e14841fc2e7695
ศรัญญู ตำลิ่ม <deathnote_boat@hotmail.com>
2017-11-14 16:15:08 +0700

    Array 4

--- Lab07/Lab07/Program.cs
+++ Lab07/Lab07/Program.cs
@@ -6,12 +6,35 @@ namespace ConsoleAppArray
     {
         static void Main(string[] args)
         {
-            int[] data = { 51, 58, 14, 20, -5, 61, 7, 21, 6 };
-            Console.WriteLine("Length of data = {0}", data.Length);
-            for (int i = 0; i < data.Length; i++)
-            {
-                Console.WriteLine(data[i]);
-            }
+            string[] array1 = { "cat", "dog", "carrot", "bird" };
+
+            //
+            // ค้นหาสมาชิกตัวแรกที่มีค่าตามกำหนด 
+            //
+            string value1 = Array.Find(array1,
+              element => element.StartsWith("car",
+              StringComparison.Ordinal));
+
+            string value2 = Array.Find(array1,
+              element => element.StartsWith("fish",
+              StringComparison.Ordinal));
+
+            //
+            // ค้นหาสมาชิกตัวแรกที่มีความยาว string ตามกำหนด
+            //
+            string value3 = Array.Find(array1,
+                element => element.Length == 3);
+
+            //
+            // ค้นหา string ที่มีความยาวไม่เกินค่าที่กำหนด
+            //
+            string[] array2 = Array.FindAll(array1,
+                element => element.Length <= 4);
+
+            Console.WriteLine(value1);
+            Console.WriteLine(value2);
+            Console.WriteLine(value3);
+            Console.WriteLine(string.Join(",", array2));
             Console.ReadLine();
         }
     }
