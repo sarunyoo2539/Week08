381a4f48987a96275e5b5181602d0e2be9e8a09b
ศรัญญู ตำลิ่ม <deathnote_boat@hotmail.com>
2017-11-14 16:44:30 +0700

    Array 2D

--- Lab07/Lab07/Program.cs
+++ Lab07/Lab07/Program.cs
@@ -1,57 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace ConsoleAppArray
+namespace MultidimensionalArray
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] ArrayA = new int[] { 1, 2, 3, 4, 5, 6 };
-            int[] ArrayB = new int[6];
-            // copy by operator =
-            ArrayB = ArrayA;
-
-            Console.WriteLine("*** Array copy by operator = ***");
-            Console.WriteLine("===== Before =====");
-            for (int i = 0; i < ArrayA.Length; i++)
+            int[,] multiplyTable = new int[12, 12];
+            multiplyTable[0, 0] = 2 * 1;
+            multiplyTable[1, 0] = 2 * 2;
+            multiplyTable[2, 0] = 2 * 3;
+            multiplyTable[3, 0] = 2 * 4;
+            multiplyTable[4, 0] = 2 * 5;
+            multiplyTable[5, 0] = 2 * 6;
+            multiplyTable[6, 0] = 2 * 7;
+            multiplyTable[7, 0] = 2 * 8;
+            multiplyTable[8, 0] = 2 * 9;
+            multiplyTable[9, 0] = 2 * 10;
+            multiplyTable[10, 0] = 2 * 11;
+            multiplyTable[11, 0] = 2 * 12;
+            for (int row = 0; row < multiplyTable.GetLength(0); row++)
             {
-                Console.WriteLine("arrayA[(0)] = {1}");
-                ArrayB[(0)] = (2);
-                ", i, ArrayA[i], ArrayB[i]);
+                for (int col = 0; col < multiplyTable.GetLength(1); col++)
+                {
+                    Console.Write("{0,5}", multiplyTable[row, col]);
+                }
+                Console.WriteLine();
             }
-            // change element 0 of ArrayA
-            ArrayA[0] = 9;
-            Console.WriteLine("===== After ======");
-            for (int i = 0; i < ArrayA.Length; i++)
-            {
-                Console.WriteLine("ArrayA[(0)] = {1}");
-                ArrayB[(0)] = (2);
-                ", i, ArrayA[i], ArrayB[i]);
-            }
-
-            // copy by method Array.Copy()
-            int[] ArrayC = new int[6];
-            Array.Copy(ArrayA, ArrayC, ArrayA.Length);
-
-            Console.WriteLine("*** Array copy by method Array.Copy() ***");
-            Console.WriteLine("===== Before =====");
-            for (int i = 0; i < ArrayA.Length; i++)
-            {
-                Console.WriteLine("ArrayA[(0)] = {1}");
-                ArrayC[(0)] = (2);
-                ", i, ArrayA[i], ArrayC[i]);
-            }
-            // change element 0 of ArrayA
-            ArrayA[0] = 1;
-            Console.WriteLine("===== After =====");
-            for (int i = 0; i < ArrayA.Length; i++)
-            {
-                Console.WriteLine("ArrayA[(0)] = {1}");
-                ArrayC[(0)] = (2);
-                ", i, ArrayA[i], ArrayC[i]);
-            }
-
-            // wait
             Console.ReadLine();
         }
     }
